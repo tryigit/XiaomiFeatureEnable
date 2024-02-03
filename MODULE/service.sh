ee025ae517ad737f162a1a3f652415fef3daada2
Yiğit <40565628+tryigit@users.noreply.github.com>
2024-02-03 11:37:32 +0300

    Update service.sh

--- MODULE/service.sh
+++ MODULE/service.sh
@@ -1,6 +1,6 @@
 # @tryigitx Please Do Not Remove Credit.
 resetprop ro.vendor.gpp.frc.support true
-resetprop ro.vendor.display.iris_x7.support true
+# resetprop ro.vendor.display.iris_x7.support
 resetprop debug.config.media.video.frc.support true
 resetprop debug.config.media.video.aie.support true
 resetprop debug.config.media.video.ais.support true
