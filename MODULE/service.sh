ac7d7c26242fe0bace7cefa1d74fe74e6d5dabd8
Yiğit <40565628+tryigit@users.noreply.github.com>
2024-02-03 07:16:22 +0300

    Create service.sh

---
+++ MODULE/service.sh
@@ -0,0 +1,9 @@
+# @tryigitx Please Do Not Remove Credit.
+resetprop ro.vendor.gpp.frc.support true
+resetprop ro.vendor.display.iris_x7.support true
+resetprop debug.config.media.video.frc.support true
+resetprop debug.config.media.video.aie.support true
+resetprop debug.config.media.video.ais.support true
+resetprop ro.vendor.media.video.frc.support true
+
+# @ ? Use Github and add features you find yourself
