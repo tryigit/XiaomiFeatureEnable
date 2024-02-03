5acb0d7bb8b19ceb19f6ec995c5fc9460afcaba8
Yiğit <40565628+tryigit@users.noreply.github.com>
2024-02-04 02:25:37 +0300

    Update service.sh

--- MODULE/service.sh
+++ MODULE/service.sh
@@ -1,9 +1,15 @@
-# @tryigitx Please Do Not Remove Credit.
+-# @tryigitx Please Do Not Remove Credit.
 resetprop ro.vendor.gpp.frc.support true
-# resetprop ro.vendor.display.iris_x7.support
 resetprop debug.config.media.video.frc.support true
 resetprop debug.config.media.video.aie.support true
 resetprop debug.config.media.video.ais.support true
 resetprop ro.vendor.media.video.frc.support true
+resetprop persist.vendor.extreme.mode true
+resetprop ro.vendor.audio.zoom.support true
+resetprop ro.vendor.audio.zoom.type 2
+resetprop ro.vendor.audio.spk.clean true
+# vendor.mi.filter.lut.enable wait
+resetprop persist.sys.privacy_camera true
+resetprop persist.vendor.camera.facetracker.support true
 
 # @ ? Use Github and add features you find yourself
